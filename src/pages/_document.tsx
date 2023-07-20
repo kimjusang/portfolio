46df7655eb5afa5a06120c7c8814a01d54b79600
kimjusang <kimjusang@gmail.com>
2023-07-20 17:05:40 +0900

    Add smooth scrolling

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
+      <Html className="scroll-smooth" lang="en">
         <Head>
           <link rel="icon" href="/favicon.ico" />
 
