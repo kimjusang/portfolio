93af0e4d9c26e96ccdcee3988459171c214b2d32
kimjusang <kimjusang@gmail.com>
2023-07-22 10:47:07 +0900

    Add type interface for the Layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,12 @@
+import React, { type ReactNode } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 
-const Layout = ({ children }) => {
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
       <div className="lg:flex lg:justify-between lg:gap-4">
