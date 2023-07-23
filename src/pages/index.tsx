8a901b363492a4bb3f03981b892623330c029a12
kimjusang <kimjusang@gmail.com>
2023-07-23 20:41:28 +0900

    Remove unnecessary _document.tsx and move the styles of html and body to globals.css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,11 @@ export default function Home() {
     <>
       <Head>
         <title>Jusang Kim</title>
+        <link rel="icon" href="/favicon.ico" />
+
+        {/* Open Graph Meta Tags */}
+
+        {/* Twitter Card Meta Tags */}
         <meta
           name="description"
           content="Jusang Kim is a software engineer who builds web applications with a focus on user experience and performance."
