ff4007632e7dd48eec148fceca43db0315756f62
kimjusang <kimjusang@gmail.com>
2023-08-05 23:36:23 +0900

    Refactor Projects section

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -1,44 +1,44 @@
-import { type FC } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
-const Projects: FC = () => {
-  interface PersonalProjectsTypes {
-    title: string;
-    description: string;
-    url: string;
-  }
+interface ProjectTypes {
+  projectTitle: string;
+  projectDescription: string;
+  projectUrl: string;
+}
 
-  const personalProjects: PersonalProjectsTypes[] = [
+const Projects = (): JSX.Element => {
+  const projects: ProjectTypes[] = [
     {
-      title: "Project Number 1",
-      description:
+      projectTitle: "Project Number 1",
+      projectDescription:
         " Video course that teaches how to build a web app with the Spotify Web API. Topics covered include the principles of REST APIs, user auth flows, Node, Express, React, Styled Components, and more.",
-      url: "https://www.newline.co/courses/build-a-spotify-connected-app",
+      projectUrl:
+        "https://www.newline.co/courses/build-a-spotify-connected-app",
     },
     {
-      title: "Project Number 2",
-      description:
+      projectTitle: "Project Number 2",
+      projectDescription:
         " Video course that teaches how to build a web app with the Spotify Web API.",
-      url: "https://www.youtube.com/",
+      projectUrl: "https://www.youtube.com/",
     },
   ];
 
   const renderTitle = (
-    title: PersonalProjectsTypes["title"],
-    url: PersonalProjectsTypes["url"]
+    projectTitle: ProjectTypes["projectTitle"],
+    projectUrl: ProjectTypes["projectUrl"]
   ): JSX.Element => {
     return (
       <h3>
         <Link
           className="group/link inline-flex items-baseline text-base font-medium leading-tight text-slate-200  hover:text-teal-300 focus-visible:text-teal-300"
-          href={url}
+          href={projectUrl}
           target="_blank"
-          aria-label="Build a Spotify Connected App"
+          aria-label={projectTitle}
         >
           <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
           <span className="inline-block">
-            {title}
+            {projectTitle}
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 20 20"
@@ -59,37 +59,79 @@ const Projects: FC = () => {
   };
 
   const renderDescription = (
-    description: PersonalProjectsTypes["description"]
-  ) => {
-    return <p className="mt-2 text-sm leading-normal">{description}</p>;
+    projectDescription: ProjectTypes["projectDescription"]
+  ): JSX.Element => {
+    return <p className="mt-2 text-sm leading-normal">{projectDescription}</p>;
+  };
+
+  const projectImage = () => {
+    return (
+      <Image
+        className="rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1"
+        alt=""
+        loading="lazy"
+        decoding="async"
+        data-nimg="1"
+        style={{ color: "transparent" }}
+        src="/favicon.ico"
+        width={200}
+        height={48}
+      />
+    );
   };
 
   const renderProjects = (): JSX.Element => {
     return (
       <>
-        {personalProjects.map((project) => (
-          <li key={project.title} className="mb-12">
+        {projects.map((project) => (
+          <li key={project.projectTitle} className="mb-12">
             <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
               <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
               <div className="z-10 sm:order-2 sm:col-span-6">
-                {renderTitle(project.title, project.url)}
-                {renderDescription(project.description)}
+                {renderTitle(project.projectTitle, project.projectUrl)}
+                {renderDescription(project.projectDescription)}
               </div>
-              <Image
-                className="rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1"
-                alt=""
-                loading="lazy"
-                decoding="async"
-                data-nimg="1"
-                style={{ color: "transparent" }}
-                src="/favicon.ico"
-                width={200}
-                height={48}
-              />
+              {projectImage()}
             </div>
           </li>
         ))}
       </>
+    );
+  };
+
+  const renderProjectArchiveLink = (): JSX.Element => {
+    return (
+      <div className="mt-12">
+        <Link
+          className="group inline-flex items-center font-semibold leading-tight text-slate-200"
+          aria-label="View Full Project Archive"
+          href="/archive"
+        >
+          <span>
+            <span className="border-b border-transparent pb-px transition group-hover:border-teal-300 motion-reduce:transition-none">
+              View Full Project&nbsp;
+            </span>
+            <span className="whitespace-nowrap">
+              <span className="border-b border-transparent pb-px transition group-hover:border-teal-300 motion-reduce:transition-none">
+                Archive
+              </span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="ml-1 inline-block h-4 w-4 shrink-0 -translate-y-px transition-transform group-hover:translate-x-2 group-focus-visible:translate-x-2 motion-reduce:transition-none"
+                aria-hidden="true"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M3 10a.75.75 0 01.75-.75h10.638L10.23 5.29a.75.75 0 111.04-1.08l5.5 5.25a.75.75 0 010 1.08l-5.5 5.25a.75.75 0 11-1.04-1.08l4.158-3.96H3.75A.75.75 0 013 10z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </span>
+          </span>
+        </Link>
+      </div>
     );
   };
 
@@ -106,37 +148,7 @@ const Projects: FC = () => {
       </div>
       <div>
         <ul className="group/list">{renderProjects()}</ul>
-        <div className="mt-12">
-          <Link
-            className="group inline-flex items-center font-medium font-semibold leading-tight text-slate-200 text-slate-200"
-            aria-label="View Full Project Archive"
-            href="/archive"
-          >
-            <span>
-              <span className="border-b border-transparent pb-px transition group-hover:border-teal-300 motion-reduce:transition-none">
-                View Full Project&nbsp;
-              </span>
-              <span className="whitespace-nowrap">
-                <span className="border-b border-transparent pb-px transition group-hover:border-teal-300 motion-reduce:transition-none">
-                  Archive
-                </span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  className="ml-1 inline-block h-4 w-4 shrink-0 -translate-y-px transition-transform group-hover:translate-x-2 group-focus-visible:translate-x-2 motion-reduce:transition-none"
-                  aria-hidden="true"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M3 10a.75.75 0 01.75-.75h10.638L10.23 5.29a.75.75 0 111.04-1.08l5.5 5.25a.75.75 0 010 1.08l-5.5 5.25a.75.75 0 11-1.04-1.08l4.158-3.96H3.75A.75.75 0 013 10z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-              </span>
-            </span>
-          </Link>
-        </div>
+        {renderProjectArchiveLink()}
       </div>
     </section>
   );
