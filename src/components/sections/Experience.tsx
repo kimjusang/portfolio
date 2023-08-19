5f8e43b7f9af6e0d7ea0b5d6ddaf784d1dba2f60
kimjusang <kimjusang@gmail.com>
2023-08-19 17:32:23 +0900

    Comment out the resume and project archive links

--- src/components/sections/Experience.tsx
+++ src/components/sections/Experience.tsx
@@ -115,38 +115,40 @@ const Experience = (): ReactNode => {
     );
   };
 
-  const renderResumeLink = () => {
-    return (
-      <div className="mt-12">
-        <Link
-          className="group inline-flex items-center font-semibold leading-tight text-slate-200"
-          aria-label="View Full Resume"
-          href="/resume.pdf"
-        >
-          <span>
-            <span className="border-b border-transparent pb-px transition group-hover:border-teal-300 motion-reduce:transition-none">
-              View Full Resume
-            </span>
-            <span className="whitespace-nowrap">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                className="ml-1 inline-block h-4 w-4 shrink-0 -translate-y-px transition-transform group-hover:translate-x-2 group-focus-visible:translate-x-2 motion-reduce:transition-none"
-                aria-hidden="true"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M3 10a.75.75 0 01.75-.75h10.638L10.23 5.29a.75.75 0 111.04-1.08l5.5 5.25a.75.75 0 010 1.08l-5.5 5.25a.75.75 0 11-1.04-1.08l4.158-3.96H3.75A.75.75 0 013 10z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-            </span>
-          </span>
-        </Link>
-      </div>
-    );
-  };
+  // TO DO: Add the resume link when it's ready to be presented
+
+  // const renderResumeLink = () => {
+  //   return (
+  //     <div className="mt-12">
+  //       <Link
+  //         className="group inline-flex items-center font-semibold leading-tight text-slate-200"
+  //         aria-label="View Full Resume"
+  //         href="/resume.pdf"
+  //       >
+  //         <span>
+  //           <span className="border-b border-transparent pb-px transition group-hover:border-teal-300 motion-reduce:transition-none">
+  //             View Full Resume
+  //           </span>
+  //           <span className="whitespace-nowrap">
+  //             <svg
+  //               xmlns="http://www.w3.org/2000/svg"
+  //               viewBox="0 0 20 20"
+  //               fill="currentColor"
+  //               className="ml-1 inline-block h-4 w-4 shrink-0 -translate-y-px transition-transform group-hover:translate-x-2 group-focus-visible:translate-x-2 motion-reduce:transition-none"
+  //               aria-hidden="true"
+  //             >
+  //               <path
+  //                 fillRule="evenodd"
+  //                 d="M3 10a.75.75 0 01.75-.75h10.638L10.23 5.29a.75.75 0 111.04-1.08l5.5 5.25a.75.75 0 010 1.08l-5.5 5.25a.75.75 0 11-1.04-1.08l4.158-3.96H3.75A.75.75 0 013 10z"
+  //                 clipRule="evenodd"
+  //               ></path>
+  //             </svg>
+  //           </span>
+  //         </span>
+  //       </Link>
+  //     </div>
+  //   );
+  // };
 
   return (
     <section
@@ -157,7 +159,7 @@ const Experience = (): ReactNode => {
       <SectionHeader sectionName="Experience" />
       <div>
         <ol className="group/list">{renderMappedWorkExperiences()}</ol>
-        {renderResumeLink()}
+        {/* {renderResumeLink()} */}
       </div>
     </section>
   );
